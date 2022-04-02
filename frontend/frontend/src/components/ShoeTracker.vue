5f26317862bb7556202e4b6dccb940ab2953db61
m-c-hill <matthewhill2595@gmail.com>
2022-04-02 16:51:59 +0100

    Basic shoe tracker front end mock-up

--- frontend/frontend/src/components/ShoeTracker.vue
+++ frontend/frontend/src/components/ShoeTracker.vue
@@ -1,5 +1,52 @@
 <template>
-  <div class="container">
-    <p>Shoe Tracker</p>
+  <div class="jumbotron vertical-center">
+    <div class="container">
+      <!-- Bootstrap CDN -->
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/sketchy/bootstrap.min.css"
+      />
+      <h1>Shoe Tracker</h1>
+      <br />
+      <div class="row">
+        <div class="col-sm-12"></div>
+        <!-- Alert message -->
+        <button type="button" class="btn btn-success">Add Shoe</button>
+        <br />
+        <table class="table">
+          <thead>
+            <tr>
+              <!-- Table header cells -->
+              <th scope="col">Brand</th>
+              <th scope="col">Model</th>
+              <th scope="col">Nickname</th>
+              <th scope="col">Distance (km)</th>
+              <th scope="col">Notes</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <!-- Dummy data to be removed -->
+              <td>Saucony</td>
+              <td>Endorphin Speed 2</td>
+              <td>Jan '22</td>
+              <td>304</td>
+              <td>Reserved for track and tempo sessions.</td>
+              <td>
+                <div class="btn-group" role="group">
+                  <button type="button" class="btn btn-info btn-sm">
+                    Update
+                  </button>
+                  <button type="button" class="btn btn-danger btn-sm">
+                    Delete
+                  </button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
--- frontend/frontend/src/router/index.js
+++ frontend/frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import SharkComp from "../components/SharkComp.vue";
+import ShoeTracker from "../components/ShoeTracker.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: "/shark",
     name: "Shark",
     component: SharkComp,
+  },
+  {
+    path: "/shoes",
+    name: "Shoes",
+    component: ShoeTracker,
   },
 ];
 
